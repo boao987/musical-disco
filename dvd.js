f7552d08bb12defddde1b2e23fba91c1a1c907dd
boao987 <schooloffranklin12@gmail.com>
2016-04-20 17:18:05 +0000

    first

---
+++ dvd.js
@@ -0,0 +1,14 @@
+var DVD = function(){
+    
+    var xcor,ycor,width,height,dx,dy;
+    
+    var add = function(){
+        
+    }
+    var moveX = function(){
+        xcor += dx;
+    }
+    var moveY = function(){
+        ycor += dy;
+    }
+}
