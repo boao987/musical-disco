fab865b6f60c329785d23a06feb775801d4af3e9
boao987 <schooloffranklin12@gmail.com>
2016-04-20 17:19:43 +0000

    js started

--- dvd.js
+++ dvd.js
@@ -1,14 +1,26 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var start = document.getElementById("animate");
+var stop = document.getElementById("stop");
+var clear = document.getElementById("clear");
+
+var frameID;
+
 var DVD = function(){
     
     var xcor,ycor,width,height,dx,dy;
     
     var add = function(){
         
-    }
-    var moveX = function(){
+    };
+    var move = function(){
         xcor += dx;
-    }
-    var moveY = function(){
         ycor += dy;
+    };
+    var isCollide = function(){
+        if(xcor<=0 || xcor >= c.width - width){
+            dx = dx * -1;
+        };
     }
 }
