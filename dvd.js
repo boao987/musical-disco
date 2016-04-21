ed05e5c6b90cc1606de7a3317c5a3af34c1b75f8
boao987 <schooloffranklin12@gmail.com>
2016-04-21 16:56:38 +0000

    hi

--- dvd.js
+++ dvd.js
@@ -1,5 +1,4 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+var svg = document.getElementById("mySVG");
 
 var start = document.getElementById("animate");
 var stop = document.getElementById("stop");
@@ -7,19 +6,33 @@ var clear = document.getElementById("clear");
 
 var frameID;
 
-var DVD = function(){
+var Ball = function(){
     
-    var xcor,ycor,width,height,dx,dy;
+    var xcor,ycor,radius,dx,dy,color;
     
-    var add = function(){
-        
+    var display = function(){
+        var ball = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        ball.setAttribute("cx", xcor);
+        ball.setAttribute("cy", ycor);
+        ball.setAttribute("r", radius);
+        ball.setAttribute("fill", "white");
+        ball.setAttribute("stroke", "black");
+    };
+    
+    var ball = function(){
+        xcor = Math.random(svg.getAttribute("width"));
+        ycor = Math.random(svg.getAttribute("height"));
+        radius = Math.random(35);
+        dx = Math.random(3);
+        dy = Math.random(3);
     };
     var move = function(){
         xcor += dx;
         ycor += dy;
     };
+    
     var isCollide = function(){
-        if(xcor<=0 || xcor >= c.width - width){
+        if(xcor<=0 || xcor >= c.width - radius){
             dx = dx * -1;
         };
     }
